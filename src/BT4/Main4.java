51d2a7f8e5b2e4c299a952b3c4beb15a70cb8917
https://github.com/thkhoa1999/HoangKhoa_59131078_HelloWorld <thkhoa1999>
2020-04-13 15:24:29 +0700

    v 1.1

--- src/BT4/Main4.java
+++ src/BT4/Main4.java
@@ -12,9 +12,9 @@ package BT4;
 public class Main4 {
     public static void main(String[] args) {
         QuanLiChuyenXe QLCX = new QuanLiChuyenXe();
-        ChuyenXeNoiThanh CXNoT1 = new ChuyenXeNoiThanh(3, 100, "MS123", "Trương Hoàng Khoa", 59131078, 3000000);
-        ChuyenXeNoiThanh CXNoT2 = new ChuyenXeNoiThanh(4, 130, "MS124", "Trần Minh Vương", 59134567, 2900000);
-        ChuyenXeNoiThanh CXNoT3 = new ChuyenXeNoiThanh(5, 150, "MS125", "Trần Văn Biểu", 59135678, 2800000);
+        ChuyenXeNoiThanh CXNoT1 = new ChuyenXeNoiThanh(9, 200, "MS123", "Trương Hoàng Khoa", 59131078, 3000000);
+        ChuyenXeNoiThanh CXNoT2 = new ChuyenXeNoiThanh(8, 190, "MS124", "Trần Minh Vương", 59134567, 2900000);
+        ChuyenXeNoiThanh CXNoT3 = new ChuyenXeNoiThanh(7, 180, "MS125", "Trần Văn Biểu", 59135678, 2800000);
         ChuyenXeNgoaiThanh CXNgT1 = new ChuyenXeNgoaiThanh("Nha Trang", 4, "MS126", "Trần Văn A", 59485689, 1000000);
         ChuyenXeNgoaiThanh CXNgT2 = new ChuyenXeNgoaiThanh("Vũng Tàu", 4, "MS127", "Trần Văn B", 59131478, 2000000);
         ChuyenXeNgoaiThanh CXNgT3 = new ChuyenXeNgoaiThanh("Ninh Bình", 4, "MS128", "Trần Văn C", 59113698, 3000000);
